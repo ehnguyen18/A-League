4992d030b4d366c3bc7e5eee383a68c73496cff2
ehnguyen18 <102787183+ehnguyen18@users.noreply.github.com>
2022-08-17 15:26:22 +1000

    Delete conn.php

--- conn.php
+++
@@ -1,9 +0,0 @@
-<!--20000820-->
-<!--Pham Gia Huy Nguyen -->
-<!--Wednesday 9:00AM-->
-<?php $dbConn = new mysqli('localhost', 'twa055', 'twa055xl', 'A_League2021_055');
-if ($dbConn->connect_error) {
-die('Connection error (' . $dbConn->connect_errno . ')'
-. $dbConn->connect_error);
-}
-?>
